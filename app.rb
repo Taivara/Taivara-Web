873beba5d1b53991f369ca8ce6d1bd4a7b7a1aa8
Zach <zach@hackazach.net>
2012-03-10 18:38:34 -0500

    adding public, views dirs

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
 	content_type :text
 	'Taivara.com'
 end
+
+get '/api',  :provides =>'json' do
+	stuff = {:blah => "you" , :stuff=>[1,2,3,4]}
+	stuff.to_json
+end
+
+get '/api/about', :provides=>'json' do
+
+end
+
+get '/api/who' , :provides=>'json' do
+
+end
+
+#more APIs to provide resume or contact info.
